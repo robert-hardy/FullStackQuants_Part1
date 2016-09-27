3209cccfe0f06a1a6d42190a7f6b28928a8d0e34
Robert Hardy <robbert@gmail.com>
2016-09-27 19:42:35 +0000

    Show the todos!

--- run_flask.py
+++ run_flask.py
@@ -3,14 +3,19 @@ from flask import (
 )
 
 
+from app.db import connect_db
 from app.flasky import the_app
 
 
 @the_app.route('/')
 def homepage():
+    conn = connect_db()
+    cur = conn.cursor()
+    cur.execute("select * from todo;")
+    rows = cur.fetchall()
     return render_template(
         'homepage.html',
-        buttons = range(10)
+        rows = rows
     )
 
 
