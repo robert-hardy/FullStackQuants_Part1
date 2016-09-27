3fcd7e1289e951583a2de47e0bf64bb19a8dd9e3
Robert Hardy <robbert@gmail.com>
2016-09-27 19:33:41 +0000

    Init the table.

--- app/db.py
+++ app/db.py
@@ -2,4 +2,19 @@ import sqlite3 as sqlite
 
 def connect_db(filename="app/db.sqlite"):
     conn = sqlite.connect(filename)
+    init_tables(conn)
     return conn
+
+
+def init_tables(conn):
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS
+            todo
+        (
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            notes TEXT
+        )
+    """)
+    conn.commit()
